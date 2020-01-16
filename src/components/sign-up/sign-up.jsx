a8ee4d19d28fbb03c79aea7355dd6e06d04ec445
harshthakur066 <harshthakur066@gmail.com>
2020-01-16 18:07:53 +0530

    added routing from home page to respective collection

--- src/components/sign-up/sign-up.jsx
+++ src/components/sign-up/sign-up.jsx
@@ -58,7 +58,7 @@ class SignUp extends React.Component {
         const { displayName, email, password, confirmPassword } = this.state;
         return (
             <div className='sign-up'>
-                <h2 className='title'>I do not have a account</h2>
+                <h2 className='title'>I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
                     <FormInput
