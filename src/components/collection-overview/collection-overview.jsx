2688a020dd8a2cf903173e4a9c4be3951d7bc830
harshthakur066 <harshthakur066@gmail.com>
2020-01-16 20:01:19 +0530

    normalized data

--- src/components/collection-overview/collection-overview.jsx
+++ src/components/collection-overview/collection-overview.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop-selectors';
+import { selectCollectionForPreview } from '../../redux/shop/shop-selectors';
 
 import CollectionPreview from '../collection-preview/collection-preview';
 
@@ -19,7 +19,7 @@ const CollectionOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionForPreview
 });
 
 
--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from 'reselect';
-
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+import { object } from 'prop-types';
 
 const selectShop = state => state.shop;
 
@@ -17,7 +10,10 @@ export const selectCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+    collections => collections[collectionUrlParam]
+);
+
+export const selectCollectionForPreview = createSelector(
+    [selectCollections],
+    collections => object.keys(collections).map(key => collections[key])
 );
