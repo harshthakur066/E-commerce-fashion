a134368db541a4d5a71801ea109ad58ce5ceef6c
harshthakur066 <harshthakur066@gmail.com>
2020-01-19 03:12:39 +0530

    changed custom-button css in js

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
 
-import './custom-button.scss';
+import { CustomButtonContainer } from './custom-button-styles';
 
-const CustomButton = ({ children, inverted, isGoogleSignIn, ...otherProps }) => (
-    <button className={
-        `${inverted ? 'inverted' : ''} 
-        ${isGoogleSignIn ? 'google-sign-in' : ''} 
-        custom-button`
-    }
-        {...otherProps}>
-        {children}
-    </button>
+const CustomButton = ({ children, ...props }) => (
+    <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
