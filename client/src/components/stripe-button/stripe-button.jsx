a7ddbf4a4a16942fce1db6198c9e0afc218762fd
harshthakur066 <harshthakur066@gmail.com>
2020-05-19 22:14:37 +0530

    added client setup for api

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStripe = price * 100;
-    const publishableKey = 'pk_test_nmdnmMeA6P1qpGsDF0Zrj8b300wfPRpu2B';
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_nmdnmMeA6P1qpGsDF0Zrj8b300wfPRpu2B";
 
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Succesful!');
-    };
+  const onToken = async (token) => {
+    try {
+      await axios.post("/checkout/payment", {
+        amount: priceForStripe,
+        token: token,
+      });
+      alert("succesful payment");
+    } catch (error) {
+      console.log("Payment Error: ", error);
+      alert("There was an issue with your payment! ");
+    }
+    // axios({
+    //   url: "/checkout/payment",
+    //   method: "post",
+    //   data: {
+    //     amount: priceForStripe,
+    //     token: token,
+    //   },
+    // })
+    //   .then((response) => {
+    //     alert("succesful payment");
+    //   })
+    //   .catch((error) => {
+    //     console.log("Payment Error: ", error);
+    //     alert(
+    //       "There was an issue with your payment! Please make sure you use the provided credit card."
+    //     );
+    //   });
+  };
 
-    return (
-        <StripeCheckout
-            label='Pay Now'
-            name=''
-            billingAddress
-            shippingAddress
-            image='https://svgshare.com/i/CUz.svg'
-            currency="INR"
-            description={`Your total is Rs.${price}`}
-            amount={priceForStripe}
-            panelLabel='Pay Now'
-            token={onToken}
-            stripeKey={publishableKey}
-        />
-    );
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name=""
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      currency="INR"
+      description={`Your total is Rs.${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
 export default StripeCheckoutButton;
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get("/service-worker.js", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
-app.post("/payment", (req, res) => {
+app.post("/checkout/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
