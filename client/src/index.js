2d1eb18da3d3b7aaa2006f8164a644f0df5aaf80
harshthakur066 <harshthakur066@gmail.com>
2020-05-21 01:23:50 +0530

    importing register for service work

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
 
-import * as serviceWorker from "./serviceWorker";
+import { register } from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
@@ -22,4 +22,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-serviceWorker.register();
+register();
