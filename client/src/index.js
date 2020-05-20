b647c45acca710cfc4e9b5beab30c9d0d8c3ddaf
harshthakur066 <harshthakur066@gmail.com>
2020-05-21 00:56:37 +0530

    runtime bundel set for service worker

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import * as serviceWorker from "./serviceWorker";
 
 import { store, persistor } from "./redux/store";
+
+import * as serviceWorker from "./serviceWorker";
 
 import "./index.css";
 import App from "./App";
